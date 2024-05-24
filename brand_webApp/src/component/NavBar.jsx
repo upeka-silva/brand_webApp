72509cac6f7ad952a1b1a29924953bcfe47c94d5
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-24 22:55:26 +0530

    navbar button and icon

--- brand_webApp/src/component/NavBar.jsx
+++ brand_webApp/src/component/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const NavBar = () => {
   return (
@@ -8,17 +9,30 @@ const NavBar = () => {
              <div className='px-2 flex  justify-between items-center w-full h-full'>
               <div className='flex items-center'>
                 <h1 className='text-3xl mr-4 font-bold  sm:text-4xl'>BRAND.</h1>
-                 <ul className='hidden gap-4 md:flex'>
+                 <ul className='hidden  md:flex'>
                    <li><a href="">Home</a></li>
                    <li><a href="">About</a></li>
                     <li><a href="">Support</a></li>
                    <li><a href="">Platforms</a></li>
                     <li><a href="">Pricing</a></li>
                  </ul>
-         </div>
+   
+                </div>
+
+                <div className='hidden md:flex gap-5 text-black mr-4'>
+                              <button className='border-none  bg-transparent' >Sign In</button>
+                              <button>Sign Up</button>
+                </div>
+
+                <div className='md:hidden'>
+
+                    <AiOutlineMenu className='mr-4'  size={20}/>
+                </div>
+
            </div>
 
-             </div>
+
+    </div>
    </>
   )
 }
